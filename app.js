e23a18989616a6798870df566df2d39969c3b65a
iamdeve <wajdanaeli@gmail.com>
2021-01-11 23:53:16 +0500

    static folder added

--- app.js
+++ app.js
@@ -36,4 +36,10 @@ app.use((error, req, res, next) => {
 	});
 });
 
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 module.exports = app;
